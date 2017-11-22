e8139e81004999f655d7f34c660ed0535ecbecaf
Jonnie Lappen <jlappen@demark.com>
2017-11-22 09:50:24 -0700

    add grading rubic as comment to index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
+// https://review.udacity.com/#!/rubrics/918/view
 ReactDOM.render(<App />, document.getElementById('root'))
